62695ab1424cc7c80589360e0ed9020ae3c17fa4
Jonas <xgv866@alumni.ku.dk>
2025-03-28 02:06:32 +0100

    upx<te api code

--- ESP8266/ESP8266-Huzzah-Feather/DisplayConfig.h
+++ ESP8266/ESP8266-Huzzah-Feather/DisplayConfig.h
@@ -1,22 +1,15 @@
-/*
-* Made by Jonas Kjeldmnd Jensen (Yunus), April 2025 
-* 
-* This code is setup to work with the Adafruit Feather HUZZAH ESP8266's pinout
-* https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/overview#uts
-*
-*/
-
 #ifndef DISPLAY_CONFIG_H
 #define DISPLAY_CONFIG_H
 
-// Initialize the display for the 4.2" ePaper module
-GxEPD2_BW<GxEPD2_420_GDEY042T81, GxEPD2_420_GDEY042T81::HEIGHT> display(GxEPD2_420_GDEY042T81(
-  /*CS=5*/   5,    // Chip Select pin
-  /*DC=*/    0,    // Data/Command pin
-  /*RST=*/   2,    // Reset pin
-  /*BUSY=*/  15    // Busy pin
-  // SCL (Serial Clock) is connected to GPIO 18
-  // SDA (Serial Data / MOSI) is connected to GPIO 23
-));
+#include <GxEPD2_BW.h>
 
-#endif
+// Pin Configuration for ESP8266 Feather HUZZAH
+#define EINK_CS   15  // Chip Select
+#define EINK_DC    4  // Data/Command
+#define EINK_RST   5  // Reset
+#define EINK_BUSY 16  // Busy
+
+// REMOVE any display definition from this file
+// DO NOT define the display here
+
+#endif // DISPLAY_CONFIG_H
