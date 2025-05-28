a279103d6a5b3c6fdb5d5b55742bcce2a2882131
Jonas <xgv866@alumni.ku.dk>
2025-05-28 13:56:14 +0200

    minor adjustments

---
+++ ESP32-Thing-4.2E-PaperDisplay+API/credentials.h
@@ -0,0 +1,11 @@
+// credentials.h
+
+
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+const char* ssid = "Yunes";
+const char* password = "123456789";
+const char* apiKey = "e9a8d76370b254b3cf1c47734d9a5ae6"; // this is the key I got sent to me in the confirmation mail from Openweather (or whatever its called)
+
+#endif
